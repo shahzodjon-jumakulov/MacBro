eea6037dd3110e5c89ac7040e6455c9111542e8b
shahzodjon-jumakulov <shahzod3137@gmail.com>
2022-11-26 20:54:25 +0500

    deploymeny

--- src/router.js
+++ src/router.js
@@ -6,6 +6,21 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: AppHome },
+        {
+            path: '/',
+            redirect: { name: 'Home' }
+        },
+        {
+            path: '/MacBro/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/MacBro/:queryParams(.*)',
+            name: 'Result',
+            component: Result,
+            props: true
+        }
     ],
 });
 
